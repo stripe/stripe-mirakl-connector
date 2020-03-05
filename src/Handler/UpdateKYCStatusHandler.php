5840fc20b758279a4188f48fa6f506576939b534
Louis Pinsard <louisp@theodo.fr>
2020-03-05 16:39:22 +0100

    Push KYC status to Mirakl

---
+++ src/Handler/UpdateKYCStatusHandler.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Handler;
+
+use App\Exception\InvalidStripeAccountException;
+use App\Message\AccountUpdateMessage;
+use App\Utils\MiraklClient;
+use App\Utils\StripeProxy;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Stripe\Account;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
+
+class UpdateKYCStatusHandler implements MessageHandlerInterface, MessageSubscriberInterface, LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    public const CURRENTLY_DUE = 'currently_due';
+    public const PENDING_VERIFICATION = 'pending_verification';
+    public const DISABLED_REASON = 'disabled_reason';
+    public const KYC_STATUS_APPROVED = 'APPROVED';
+    public const KYC_STATUS_REFUSED = 'REFUSED';
+    public const KYC_STATUS_PENDING_APPROVAL = 'PENDING_APPROVAL';
+    public const KYC_STATUS_PENDING_SUBMISSION = 'PENDING_SUBMISSION';
+
+    /**
+     * @var MiraklClient
+     */
+    private $miraklClient;
+
+    /**
+     * @var StripeProxy
+     */
+    private $stripeProxy;
+
+    public function __construct(MiraklClient $miraklClient, StripeProxy $stripeProxy)
+    {
+        $this->miraklClient = $miraklClient;
+        $this->stripeProxy = $stripeProxy;
+    }
+
+    public function __invoke(AccountUpdateMessage $message)
+    {
+        $messagePayload = $message->getContent()['payload'];
+        $this->logger->info('Received Stripe `acount.updated` webhook. Updating KYC status.', $messagePayload);
+
+        $stripeAccount = $this->stripeProxy->accountRetrieve($messagePayload['stripeUserId']);
+
+        $this->miraklClient->patchShops([
+            'shopId' => $messagePayload['miraklShopId'],
+            'kyc' => [
+                'status' => $this->getKYCStatus($stripeAccount),
+            ]
+        ]);
+    }
+
+    public static function getHandledMessages(): iterable
+    {
+        yield AccountUpdateMessage::class => [
+            'from_transport' => 'update_kyc_status',
+        ];
+    }
+
+    private function getKYCStatus(Account $stripeAccount): string
+    {
+        $requirements = $stripeAccount->requirements;
+
+        if (count($requirements[self::CURRENTLY_DUE]) > 0) {
+            return self::KYC_STATUS_PENDING_SUBMISSION;
+        }
+
+        if (count($requirements[self::PENDING_VERIFICATION]) > 0) {
+            return self::KYC_STATUS_PENDING_APPROVAL;
+        }
+
+        if (
+            $requirements[self::DISABLED_REASON] !== ''
+            && strpos($requirements[self::DISABLED_REASON], 'rejected') === 0
+        ) {
+            return self::KYC_STATUS_REFUSED;
+        }
+
+        if ($requirements[self::DISABLED_REASON] !== '') {
+            return self::KYC_STATUS_PENDING_APPROVAL;
+        }
+
+        if ($stripeAccount->payouts_enabled && $stripeAccount->charges_enabled) {
+            return self::KYC_STATUS_APPROVED;
+        }
+
+        $this->logger->error(sprintf('Could not calculate KYC status for account %s', $stripeAccount->id), [
+            'requirements' => $requirements,
+        ]);
+
+        throw new InvalidStripeAccountException();
+    }
+}
---
+++ tests/Handler/UpdateKYCStatusHandlerTest.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Tests\MessageHandler;
+
+use App\Exception\InvalidStripeAccountException;
+use App\Factory\MiraklPatchShopFactory;
+use App\Handler\UpdateAccountLoginLinkHandler;
+use App\Handler\UpdateKYCStatusHandler;
+use App\Message\AccountUpdateMessage;
+use App\Utils\MiraklClient;
+use App\Utils\StripeProxy;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+use Stripe\Account;
+
+class UpdateKYCStatusHandlerTest extends TestCase
+{
+    /**
+     * @var MiraklClient
+     */
+    private $miraklClient;
+
+    /**
+     * @var StripeProxy
+     */
+    private $stripeProxy;
+
+    /**
+     * @var MiraklPatchShopFactory
+     */
+    private $patchFactory;
+
+    /**
+     * @var UpdateAccountLoginLinkHandler
+     */
+    private $handler;
+
+    protected function setUp(): void
+    {
+        $this->miraklClient = $this->createMock(MiraklClient::class);
+        $this->stripeProxy = $this->createMock(StripeProxy::class);
+
+        $this->handler = new UpdateKYCStatusHandler($this->miraklClient, $this->stripeProxy);
+
+        $logger = new NullLogger();
+
+        $this->handler->setLogger($logger);
+    }
+
+    /**
+     * @dataProvider getCorrectKYCStatusData
+     */
+    public function testShouldUpdateCorrectKYCStatus($requirements, $payoutEnabled, $chargesEnabled, $KYCStatus)
+    {
+        $stripeAccount = new Account();
+        $stripeAccount->requirements = $requirements;
+        $stripeAccount->payouts_enabled = $payoutEnabled;
+        $stripeAccount->charges_enabled = $chargesEnabled;
+
+        $this->stripeProxy
+            ->expects($this->once())
+            ->method('accountRetrieve')
+            ->with('acct_stripe_account')
+            ->willReturn($stripeAccount);
+
+        $this->miraklClient
+            ->expects($this->once())
+            ->method('patchShops')
+            ->with([
+                'shopId' => 2000,
+                'kyc' => [
+                    'status' => $KYCStatus,
+                ]
+            ]);
+
+        $message = new AccountUpdateMessage(2000, 'acct_stripe_account');
+
+        $handler = $this->handler;
+        $handler($message);
+    }
+
+    public function getCorrectKYCStatusData(): \Generator
+    {
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => ['not_empty'],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => '',
+            ],
+            'payoutEnabled' => false,
+            'chargesEnabled' => false,
+            'KYCStatus' => UpdateKYCStatusHandler::KYC_STATUS_PENDING_SUBMISSION
+        ];
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => ['not_empty'],
+                UpdateKYCStatusHandler::DISABLED_REASON => '',
+            ],
+            'payoutEnabled' => false,
+            'chargesEnabled' => true,
+            'KYCStatus' => UpdateKYCStatusHandler::KYC_STATUS_PENDING_APPROVAL
+        ];
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => 'rejected.test',
+            ],
+            'payoutEnabled' => true,
+            'chargesEnabled' => false,
+            'KYCStatus' => UpdateKYCStatusHandler::KYC_STATUS_REFUSED
+        ];
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => 'not_rejected',
+            ],
+            'payoutEnabled' => true,
+            'chargesEnabled' => false,
+            'KYCStatus' => UpdateKYCStatusHandler::KYC_STATUS_PENDING_APPROVAL
+        ];
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => '',
+            ],
+            'payoutEnabled' => true,
+            'chargesEnabled' => true,
+            'KYCStatus' => UpdateKYCStatusHandler::KYC_STATUS_APPROVED
+        ];
+    }
+
+    /**
+     * @dataProvider getInvalidAccountData
+     */
+    public function testShouldThrowWhenInvalidAccount($requirements, $payoutEnabled, $chargesEnabled)
+    {
+        $stripeAccount = new Account();
+        $stripeAccount->requirements = $requirements;
+        $stripeAccount->payouts_enabled = $payoutEnabled;
+        $stripeAccount->charges_enabled = $chargesEnabled;
+
+        $this->stripeProxy
+            ->expects($this->once())
+            ->method('accountRetrieve')
+            ->with('acct_stripe_account')
+            ->willReturn($stripeAccount);
+
+        $this->expectException(InvalidStripeAccountException::class);
+
+        $message = new AccountUpdateMessage(2000, 'acct_stripe_account');
+
+        $handler = $this->handler;
+        $handler($message);
+    }
+
+    public function getInvalidAccountData(): \Generator
+    {
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => '',
+            ],
+            'payoutEnabled' => false,
+            'chargesEnabled' => false,
+        ];
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => '',
+            ],
+            'payoutEnabled' => false,
+            'chargesEnabled' => true,
+        ];
+        yield [
+            'requirements' => [
+                UpdateKYCStatusHandler::CURRENTLY_DUE => [],
+                UpdateKYCStatusHandler::PENDING_VERIFICATION => [],
+                UpdateKYCStatusHandler::DISABLED_REASON => '',
+            ],
+            'payoutEnabled' => true,
+            'chargesEnabled' => false,
+        ];
+    }
+
+    public function testGetHandledMessage()
+    {
+        $handledMessage = iterator_to_array(UpdateKYCStatusHandler::getHandledMessages());
+        $this->assertEquals([
+            AccountUpdateMessage::class => [
+                'from_transport' => 'update_kyc_status',
+            ],
+        ], $handledMessage);
+    }
+}
