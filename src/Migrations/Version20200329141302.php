4eb0142f7850dc00c2b47b7a0ef4af0b2c82deaf
ablanchard <ablanchard@mirakl.com>
2020-04-08 13:03:17 +0200

    Add index

--- src/Migrations/Version20200329141302.php
+++ src/Migrations/Version20200329141302.php
@@ -24,6 +24,7 @@ final class Version20200329141302 extends AbstractMigration
 
         $this->addSql('CREATE SEQUENCE mirakl_refund_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE mirakl_refund (id INT NOT NULL, amount INT NOT NULL, currency VARCHAR(255) NOT NULL, mirakl_refund_id VARCHAR(255) NOT NULL, mirakl_order_id VARCHAR(255) NOT NULL, stripe_refund_id VARCHAR(255) DEFAULT NULL, stripe_reversal_id VARCHAR(255) DEFAULT NULL, status VARCHAR(255) NOT NULL, failed_reason VARCHAR(1024) NULL, creation_datetime TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, modification_datetime TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_B19DB5F748568E43 ON mirakl_refund (mirakl_refund_id)');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +33,7 @@ final class Version20200329141302 extends AbstractMigration
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA IF NOT EXISTS public');
+        $this->addSql('DROP INDEX UNIQ_B19DB5F748568E43');
         $this->addSql('DROP SEQUENCE mirakl_refund_id_seq CASCADE');
         $this->addSql('DROP TABLE mirakl_refund');
     }
