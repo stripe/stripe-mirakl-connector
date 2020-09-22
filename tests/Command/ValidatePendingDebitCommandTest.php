8452e54d36c3292de269a7cf701f6f35239399bd
Romain Barberi <romain.barberi@gmail.com>
2020-09-22 19:01:41 +0200

    begin test on validate pending debit command

---
+++ tests/Command/ValidatePendingDebitCommandTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Tests\Command;
+
+use App\Entity\StripePayment;
+use App\Entity\StripeTransfer;
+use App\Repository\StripePaymentRepository;
+use Hautelook\AliceBundle\PhpUnit\RecreateDatabaseTrait;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @group integration
+ */
+class ValidatePendingDebitCommandTest extends KernelTestCase
+{
+    use RecreateDatabaseTrait;
+
+    /**
+     * @var \Symfony\Component\Console\Command\Command
+     */
+    protected $command;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $stripePaymentRepository;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $application = new Application($kernel);
+        $this->command = $application->find('connector:validate:pending-debit');
+
+        $this->doctrineReceiver = self::$container->get('messenger.transport.process_transfers');
+
+        $this->stripePaymentRepository = $this->getMockBuilder(StripePaymentRepository::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['findPendingPaymentByOrderIds', 'persistAndFlush'])
+            ->getMock();
+    }
+
+    public function testNominalExecute()
+    {
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute([
+            'command' => $this->command->getName(),
+        ]);
+
+        $stripeTransfersPending = $this->stripeTransferRepository->findBy([
+            'status' => StripeTransfer::TRANSFER_PENDING
+        ]);
+
+        $this->assertEquals(0, $commandTester->getStatusCode());
+        $this->assertCount(1, $this->doctrineReceiver->getSent());
+        $this->assertEquals(4, count($stripeTransfersPending));
+    }
+
+    public function testValidTransferAmount()
+    {
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute([
+            'command' => $this->command->getName()
+        ]);
+
+        $stripeTransfer = $this->stripeTransferRepository->findOneBy([
+          'miraklId' => 'order_4'
+        ]);
+
+        // Transfer amount should be 24 - 5 (commission) + 1 + 2 (shipping taxes) + 1 + 2 (taxes)
+        $this->assertEquals(2500, $stripeTransfer->getAmount());
+    }
+
+    public function testRetryFailedTransfer()
+    {
+        $commandTester = new CommandTester($this->command);
+
+        $commandTester->execute([
+            'command' => $this->command->getName(),
+            'mirakl_order_ids' => ['order_failed_transfer', 'new_order_1']
+        ]);
+
+        // Failed transfer should be retried
+        $retriedStripeTransfer = $this->stripeTransferRepository->findOneBy([
+            'miraklId' => 'order_failed_transfer'
+        ]);
+        $this->assertEquals(StripeTransfer::TRANSFER_PENDING, $retriedStripeTransfer->getStatus());
+
+        // Subsequent transfer should not have failed
+        $subsequentStripeTransfer = $this->stripeTransferRepository->findOneBy([
+            'miraklId' => 'new_order_1'
+        ]);
+        $this->assertEquals(StripeTransfer::TRANSFER_PENDING, $subsequentStripeTransfer->getStatus());
+
+        // Two messages should be sent
+        $this->assertCount(2, $this->doctrineReceiver->getSent());
+    }
+
+    public function testRetryCreatedTransfer()
+    {
+        $commandTester = new CommandTester($this->command);
+
+        $commandTester->execute([
+            'command' => $this->command->getName(),
+            'mirakl_order_ids' => ['order_created_transfer', 'new_order_2']
+        ]);
+
+        // Created transfer shouldn't be retried
+        $stripeTransfer = $this->stripeTransferRepository->findOneBy([
+            'miraklId' => 'order_created_transfer'
+        ]);
+        $this->assertEquals(StripeTransfer::TRANSFER_CREATED, $stripeTransfer->getStatus());
+
+        // Subsequent transfer should not have failed
+        $subsequentStripeTransfer = $this->stripeTransferRepository->findOneBy([
+            'miraklId' => 'new_order_2'
+        ]);
+        $this->assertEquals(StripeTransfer::TRANSFER_PENDING, $subsequentStripeTransfer->getStatus());
+
+        // Only one message should be sent
+        $this->assertCount(1, $this->doctrineReceiver->getSent());
+    }
+
+    public function testFailedTransferAlreadyProcessed()
+    {
+        $commandTester = new CommandTester($this->command);
+
+        $commandTester->execute([
+            'command' => $this->command->getName(),
+            'mirakl_order_ids' => ['order_already_processed']
+        ]);
+
+        // Transfer should be transitioned back to created
+        $stripeTransfer = $this->stripeTransferRepository->findOneBy([
+            'miraklId' => 'order_already_processed'
+        ]);
+        $this->assertEquals(StripeTransfer::TRANSFER_CREATED, $stripeTransfer->getStatus());
+
+        // No message should be sent
+        $this->assertCount(0, $this->doctrineReceiver->getSent());
+    }
+}
--- tests/MiraklMockedHttpClient.php
+++ tests/MiraklMockedHttpClient.php
@@ -55,6 +55,15 @@ class MiraklMockedHttpClient extends MockHttpClient
                     return new MockResponse($this->getJsonAlreadyExistingMiraklInvoices());
                 case '/invoices?shop=4':
                     return new MockResponse($this->getJsonFailedTransferMiraklInvoices());
+                case '/payment/debit':
+                    switch ($method) {
+                        case 'GET':
+                            return new MockResponse($this->getPendingRefunds());
+                        case 'PUT':
+                            return $this->mockRefundValidation($options);
+                    }
+                    return new MockResponse($this->getEmptyJson());
+                    break;
                 case '/payment/refund':
                     switch ($method) {
                         case 'GET':
@@ -427,5 +436,88 @@ class MiraklMockedHttpClient extends MockHttpClient
             ],
             'total_count' => 2,
           ]);
+    }
+
+    private function getPendingPayment()
+    {
+        return json_encode([
+            'orders' => [
+                'order' => [
+                    [
+                        'amount' => 100,
+                        'currency_iso_code' => 'USD',
+                        'customer_id' => 'Customer_id_001',
+                        'order_commercial_id' => 'Order_Amount_Breakdown_0002',
+                        'order_id' => 'Order_Amount_Breakdown_0002-A',
+                        'order_lines' => [
+                            'order_line' => [
+                                [
+                                    'offer_id' => '2130',
+                                    'order_line_amount' => 20,
+                                    'order_line_id' => 'Order_Amount_Breakdown_0002-A-1',
+                                    'order_line_quantity' => 10,
+                                    'refunds' => [
+                                        'refund' => [
+                                            [
+                                                'amount' => 10,
+                                                'id' => '6666',
+                                            ],
+                                            [
+                                                'amount' => 5,
+                                                'id' => '1105',
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                                [
+                                    'offer_id' => '2130',
+                                    'order_line_amount' => 80,
+                                    'order_line_id' => 'Order_Amount_Breakdown_0002-A-2',
+                                    'order_line_quantity' => 10,
+                                    'refunds' => [
+                                        'refund' => [
+                                            [
+                                                'amount' => 8,
+                                                'id' => '1106',
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'payment_workflow' => 'PAY_ON_ACCEPTANCE',
+                        'shop_id' => '2000',
+                    ],
+                    [
+                        'amount' => 100,
+                        'currency_iso_code' => 'EUR',
+                        'customer_id' => 'Customer_id_001',
+                        'order_commercial_id' => 'Order_Amount_Breakdown_0003',
+                        'order_id' => 'Order_Amount_Breakdown_0002-A',
+                        'order_lines' => [
+                            'order_line' => [
+                                [
+                                    'offer_id' => '1599',
+                                    'order_line_amount' => 100,
+                                    'order_line_id' => 'Order_Amount_Breakdown_0003-A-1',
+                                    'order_line_quantity' => 1,
+                                    'refunds' => [
+                                        'refund' => [
+                                            [
+                                                'amount' => 10,
+                                                'id' => '1199',
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'payment_workflow' => 'PAY_ON_ACCEPTANCE',
+                        'shop_id' => '2002',
+                    ],
+                ],
+            ],
+            'total_count' => 2,
+        ]);
     }
 }
