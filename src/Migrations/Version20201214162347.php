08295e8d4b5b29e6908b940e8a1044fc71851734
Kevin Raynel <kraynel@users.noreply.github.com>
2020-12-14 18:23:33 +0100

    Rename sequence to match code naming (#45)

---
+++ src/Migrations/Version20201214162347.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201214162347 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Rename payment_id sequence to match previous renaming of object in code';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER SEQUENCE IF EXISTS stripe_payment_id_seq RENAME TO stripe_charge_id_seq;');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER SEQUENCE IF EXISTS stripe_charge_id_seq RENAME TO stripe_payment_id_seq;');
+    }
+}
