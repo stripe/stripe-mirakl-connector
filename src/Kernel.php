6f61c8a58c34ef3c8bf9093eb8179b6ac7f26f4f
Anuj Kumar <tejpal@cnetric.com>
2023-06-19 13:38:23 +0530

    updated;

--- src/Kernel.php
+++ src/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends BaseKernel
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 //yield new $class();
-                
                 yield $class;
             }
         }
