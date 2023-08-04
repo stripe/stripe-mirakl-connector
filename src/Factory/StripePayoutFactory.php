3cbc6985df9b13d2a5dc76d0d58788c5ee2b7442
Anuj Kumar <anuj.kumar@tryzens.com>
2023-08-04 21:45:28 +0530

    fixed the space format issue.

--- src/Factory/StripePayoutFactory.php
+++ src/Factory/StripePayoutFactory.php
@@ -100,10 +100,11 @@ class StripePayoutFactory implements LoggerAwareInterface
     {
         $amount = $invoice['summary']['amount_transferred'] ?? 0;
         $transactions = $mclient->getTransactionsForInvoce($invoice['invoice_id']);
-        if($this->enablePaymentTaxSplit){
+        if ($this->enablePaymentTaxSplit){
             $total_tax = $this->findTotalOrderTax($transactions);
             $amount = $amount - $total_tax;
         }
+        
         $amount = gmp_intval((string) ($amount * 100));
         if ($amount <= 0) {
             throw new InvalidArgumentException(sprintf(StripePayout::PAYOUT_STATUS_REASON_INVALID_AMOUNT, $amount));
